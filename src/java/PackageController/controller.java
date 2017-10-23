bbe536c43719cb41b47e8564e64f0f04319ccdb5
Luis Javier Acosta Munoz <jennyluis1@hotmail.com>
2017-10-23 11:01:49 -0500

    inicio de variables en el servlet

---
+++ src/java/PackageController/controller.java
@@ -0,0 +1,13 @@
+package PackageController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+    
+    @RequestMapping("*")    
+    public String showAbout(){
+        return "about";
+    }
+}
